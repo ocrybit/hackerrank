30e0c65e50af51d20936c72dadf61946183e2afe
OK Rabbit <ocrybit@gmail.com>
2018-12-31 01:31:18 +0100

    Project Euler #6: Sum square difference

--- contests/projecteuler/euler006.py
+++ contests/projecteuler/euler006.py
@@ -2,9 +2,9 @@
 import sys
 
 def solve(n):
-    sum1 = (((1 + n) * n) // 2) ** 2
-    sum2 = (n * (n + 1) * ((2 * n) + 1)) // 6
-    return abs(sum1 - sum2)
+    sum_square = (((1 + n) * n) // 2) ** 2
+    square_sum = (n * (n + 1) * ((2 * n) + 1)) // 6
+    return sum_square - square_sum
 
 t = int(input().strip())
 for a0 in range(t):
