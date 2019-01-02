2dded2e67a22a388896407d159cb4d8d1d3c7f02
OK Rabbit <ocrybit@gmail.com>
2019-01-02 08:22:39 +0100

    Project Euler #10: Summation of primes

--- contests/projecteuler/euler010.py
+++ contests/projecteuler/euler010.py
@@ -2,30 +2,27 @@
 import math
 import sys
 
-def getPrimeArr(n):
+def getPrimeSums(n):
     nums = [None] * (n + 1)
     nums[0] = False
     nums[1] = False
+    prime_sum = 0
+    sums = [0, 0]
     i = 2
     while i <= n:
         if nums[i] != False:
             nums[i] = True
+            prime_sum += i
             prod = i * 2
             while prod <= n:
                 nums[prod] = False
                 prod += i
+        sums.append(prime_sum)
         i += 1
-    return nums
-
-def getPrimeSums(n):
-    primes_sum = 0
-    sums = []
-    for i, v in enumerate(getPrimeArr(n)):
-        if v: primes_sum += i
-        sums.append(primes_sum)    
     return sums
 
-sums = getPrimeSums(1000000)
+
+sums = getPrimeSums(10 ** 6)
 
 def solve(n): return sums[n]
     
