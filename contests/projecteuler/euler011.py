553f4ba1def016379227fbe967a040c432fb102f
OK Rabbit <ocrybit@gmail.com>
2019-01-05 09:38:45 +0100

    Project Euler #11: Largest product in a grid

--- contests/projecteuler/euler011.py
+++ contests/projecteuler/euler011.py
@@ -4,11 +4,7 @@ import sys
 import itertools
 
 def solve (grid):
-    def prod(i, i2, a1, a2):
-        p = grid[i][i2]
-        for n in range(1, 4): p *= grid[i + (n * a1)][i2 + (n * a2)]
-        return p
-    
+    def prod(i, i2, a1, a2, n = 0): return 1 if n > 3 else grid[i + (n * a1)][i2 + (n * a2)] * prod(i, i2, a1, a2, n + 1)
     mx = 0
     for i, i2 in itertools.product(range(0, 20), range(0, 17)):
         mx = max(mx, prod(i, i2, 0, 1), prod(i2, i, 1, 0))
