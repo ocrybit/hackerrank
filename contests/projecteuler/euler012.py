df33b75763b5fe603def1cf8be38869184ea57cf
OK Rabbit <ocrybit@gmail.com>
2019-01-06 07:14:22 +0100

    Project Euler #12: Highly divisible triangular number

--- contests/projecteuler/euler012.py
+++ contests/projecteuler/euler012.py
@@ -2,8 +2,66 @@
 
 import sys
 
+def getPrimes(n):
+    nums = [True] * (n + 1)
+    nums[0] = False
+    nums[1] = False
+    i = 2
+    while i ** 2 <= n:
+        if nums[i] != False:
+            nums[i] = True
+            prod = i * 2
+            while prod <= n:
+                nums[prod] = False
+                prod += i
+        i += 1
+    primes = []
+    for num, isPrime in enumerate(nums):
+        if isPrime:
+            primes.append(num)
+    return primes
+
+primes = getPrimes(41039)
+
+def getFactors(n):
+    factors = {}
+    i = 0
+    p = primes[i]
+    while i < n:
+        if n % p == 0:
+            n //= p
+            factors.setdefault(p,0)
+            factors[p] += 1
+        else:
+            i += 1
+            p = primes[i]
+    return factors
+
+def combi(factors):
+    c = 1
+    for n in factors.values():
+        c *= (n + 1)
+    return c
+
+def getPrimeFactors(n):
+    max_num_factors = 1
+    factors_map = [0]
+    triangle_sum = 1
+    num = 2
+    while max_num_factors <= n:
+        triangle_sum += num
+        factors = getFactors(triangle_sum)
+        c = combi(factors)
+        if max_num_factors < c:
+            max_num_factors = max(max_num_factors, c)
+            while len(factors_map) < max_num_factors:
+                factors_map.append(triangle_sum)
+        num += 1
+    return factors_map
+
+factors_map = getPrimeFactors(10 ** 3)
 def solve(n):
-    return n
+    return factors_map[n]
 
 t = int(input().strip())
 for a0 in range(t):
